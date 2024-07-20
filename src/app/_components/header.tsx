4b8d6df0dad711081a84a03f221c53bda095444d
Uttkarsh Bansal <uttkarashbansal@UTTKARSH-MAC-5.local>
2024-07-20 21:56:41 +0530

    header sub-header skeleton

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,23 +1,29 @@
-export function Header(){
-    return <nav>        
-        <div className="flex justify-end">
-            <div>Help</div>
-            <div>Orders & Returns</div>
-            <div>Hi, John</div>
+export function Header() {
+  return (
+    <nav>
+      <div className="flex justify-end">
+        <div>Help</div>
+        <div>Orders & Returns</div>
+        <div>Hi, John</div>
+      </div>
+      <div className="flex justify-between">
+        <h1>ECOMMERCE</h1>
+        <div className="flex">
+          <div>Categories</div>
+          <div>Sale</div>
+          <div>Clearance</div>
+          <div>New stock</div>
+          <div>Trending</div>
         </div>
-        <div className="flex justify-between">
-            <h1>ECOMMERCE</h1>
-            <div className="flex">
-                <div>Categories</div>
-                <div>Sale</div>
-                <div>Clearance</div>
-                <div>New stock</div>
-                <div>Trending</div>
-            </div>
-            <div>
-            <div>Search</div>
-            <div>Cart</div>
+        <div className="flex">
+          <div>Search</div>
+          <div>Cart</div>
         </div>
-        </div>        
+      </div>
+
+      <div className="flex justify-center">
+        <div>Get 10% off on business sign up</div>
+      </div>
     </nav>
+  );
 }
