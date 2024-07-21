abcec40d0ead898c132015dffb41e3d6010ca948
Uttkarsh Bansal <uttkarashbansal@UTTKARSH-MAC-5.local>
2024-07-21 23:52:20 +0530

    local storage fix

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,32 +1,40 @@
-'use client'
+"use client";
 import Image from "next/image";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export function Header() {
-  
-  // let userAuth;
-  // useEffect(()=>{
-  //   userAuth = localStorage.getItem("userAuth")
-  //   if(userAuth){
-  //     userAuth = JSON.parse(userAuth)
-  //   }
-  // },[])
+  // const [userAuth, setUserAuth] = useState(null)
+  const [userAuth, setUserAuth] = useState({
+    userId: "",
+    name: "",
+  })
 
-  let userAuth = localStorage.getItem("userAuth")
-  if(userAuth){
-    userAuth = JSON.parse(userAuth)
-  }
-  
+  useEffect(() => {
+    let userAuth = localStorage.getItem("userAuth");
+    if (userAuth) {
+      userAuth = JSON.parse(userAuth);
+      // let parsedUserAuth: { userId?: string } | null = JSON.parse(userAuth);
+      setUserAuth(userAuth);
+    }
+  }, []);
+
   return (
     <nav>
-      <div className="flex justify-end py-3 px-[40px] text-xs font-normal">
+      <div className="flex justify-end px-[40px] py-3 text-xs font-normal">
         <div className="px-[20px]">Help</div>
         <div className="px-[20px]">Orders & Returns</div>
-        <div className="px-[20px]">{userAuth ? (userAuth as { name?: string }).name || "username" : "username"}</div>
+        <div className="px-[20px]">
+          {/* {userAuth
+            ? (userAuth as { name?: string }).name || "username"
+            : "username"} */}
+            {userAuth
+            ? userAuth.name
+            : "username"}
+        </div>
       </div>
       <div className="flex justify-between px-[40px] pb-[22px]">
         <h1 className="text-2xl font-bold">ECOMMERCE</h1>
-        <div className="flex font-semibold text-base">
+        <div className="flex text-base font-semibold">
           <div className="pe-8">Categories</div>
           <div className="pe-8">Sale</div>
           <div className="pe-8">Clearance</div>
@@ -34,18 +42,31 @@ export function Header() {
           <div className="pe-8">Trending</div>
         </div>
         <div className="flex">
-          <div className="pe-8"><Image src="/Search.svg" width={32} height={32} alt="search"/></div>
-          <div><Image src="/Cart.svg" width={32} height={32} alt="cart"/></div>
+          <div className="pe-8">
+            <Image src="/Search.svg" width={32} height={32} alt="search" />
+          </div>
+          <div>
+            <Image src="/Cart.svg" width={32} height={32} alt="cart" />
+          </div>
         </div>
       </div>
 
       <div className="flex justify-center bg-[#F4F4F4] py-2.5">
         <div className="flex">
-          <Image  src="/left-arrow.svg" width={16} height={16} alt="left arrow"/>
-            <div className="px-6 ">Get 10% off on business sign up</div>
-          <Image  src="/right-arrow.svg" width={16} height={16} alt="right arrow"/>
+          <Image
+            src="/left-arrow.svg"
+            width={16}
+            height={16}
+            alt="left arrow"
+          />
+          <div className="px-6">Get 10% off on business sign up</div>
+          <Image
+            src="/right-arrow.svg"
+            width={16}
+            height={16}
+            alt="right arrow"
+          />
         </div>
-        
       </div>
     </nav>
   );
